2422a5ace340e43b1ea956a7b2e63ef5183be470
acervall <ameliecervall@gmail.com>
2023-03-09 15:04:51 +0100

    add nnew products

--- src/views/CreateNewView.vue
+++ src/views/CreateNewView.vue
@@ -38,7 +38,7 @@
         <div id="media" class="componentCard">
           <label for="image">Image</label>
           <div>
-            <img :src="newImg" alt="" width="200" />
+            <img :src="product.image" alt="" width="200" />
             <div>
               <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
             </div>
@@ -56,7 +56,7 @@
           <div>
             <label for="sizeguide">Size Guide</label>
             <div class="textFieldInput">
-              <input type="text" id="sizeguide" v-model="product.sizeguide" />
+              <input type="text" id="sizeguide" v-model="product.size_guide" />
             </div>
           </div>
 
@@ -74,7 +74,7 @@
           <div id="status">
             <label for="active">Product status</label>
             <div class="textFieldInput">
-              <select v-model="active" id="active">
+              <select v-model="product.active" id="active">
                 <option value="1">Active</option>
                 <option value="0">Inactive</option>
               </select>
@@ -85,7 +85,7 @@
             <label for="price">Price</label>
             <div class="textFieldInput" style="flex-direction: row">
               <input type="text" id="price" size="7" v-model="product.price" />
-              <div>kr</div>
+              <div>$</div>
             </div>
           </div>
 
@@ -99,12 +99,77 @@
             <div class="border-t pt-2 border-gray-200">
               <label for="category">Category</label>
               <div class="textFieldInput">
-                <select v-model="product.category">
-                  <option value="electronics">electronics</option>
-                  <option value="jewelery">jewelery</option>
-                  <option value="men's clothing">men's clothing</option>
-                  <option value="women's clothing">women's clothing</option>
+                <select v-model="product.category2">
+                  <option value="Man">Man</option>
+                  <option value="Woman">Woman</option>
+                  <option value="Accessories">Accessories</option>
                 </select>
+              </div>
+            </div>
+            <div class="border-t pt-2 border-gray-200">
+              <label for="color">Color</label>
+              <div id="color" class="grid grid-cols-6">
+                <div v-for="col in color" :key="col">
+                  <div class="rounded-full border border-gray-200 w-4 h-4 m-1" :style="{ backgroundColor: col }" />
+                </div>
+              </div>
+
+              <div class="border-t pt-2 border-gray-200">
+                <label for="size">Size</label>
+                <div id="checkboxes">
+                  <div>
+                    <input type="checkbox" value="XS" />
+                    <label for="XS">XS</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="S" />
+                    <label for="S">S</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="M" />
+                    <label for="M">M</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="L" />
+                    <label for="L">L</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="XL" />
+                    <label for="XL">XL</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="32" />
+                    <label for="32">32</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="34" />
+                    <label for="34">34</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="36" />
+                    <label for="36">36</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="38" />
+                    <label for="38">38</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="40" />
+                    <label for="40">40</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="42" />
+                    <label for="42">42</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="44" />
+                    <label for="44">44</label>
+                  </div>
+                  <div>
+                    <input type="checkbox" value="One Size" />
+                    <label for="One Size">One Size</label>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -114,7 +179,7 @@
 
     <div class="flex justify-end">
       <div>
-        <button class="button-68 greenW" role="button" @click="putProduct()">Save Product</button>
+        <button class="button-68 greenW" role="button" @click="postProduct()">Add New Product</button>
       </div>
     </div>
   </main>
@@ -135,21 +200,65 @@
 
     data() {
       return {
+        urlApi: 'http://SITsApi.us-east-1.elasticbeanstalk.com/',
+        color: ['White', 'Gray', 'Black', 'Brown', 'Green', 'Red', 'Yellow', 'Blue'],
         product: {
+          category2: null,
           title: null,
           price: null,
           description: null,
-          category: null,
+          image: null,
           stock: null,
+          active: null,
+          material: null,
+          size_guide: null,
+          info: null,
+          color: 'White',
+          size: 'XS',
         },
       }
     },
     created() {},
-    methods: {},
+    methods: {
+      async postProduct() {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            title: `'${this.product.title}'`,
+            price: parseInt(this.product.price),
+            description: `'${this.product.description}'`,
+            category2: `'${this.product.category2}'`,
+            image: `'${this.product.image}'`,
+            stock: parseInt(this.product.stock),
+            active: parseInt(this.product.active),
+            material: `'${this.product.material}'`,
+            size_guide: `'${this.product.size_guide}'`,
+            info: `'${this.product.info}'`,
+            color: `'${this.product.color}'`,
+            size: `'${this.product.size}'`,
+          }),
+        }
+        const response = await fetch(`${this.urlApi}products`, requestOptions)
+        const data = await response.json()
+        console.log(data)
+      },
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+  #checkboxes {
+    div {
+      display: flex;
+      flex-direction: row;
+    }
+  }
+
+  #color {
+    display: grid;
+  }
+
   main {
     width: 90%;
     max-width: max-content;
@@ -186,3 +295,19 @@
     }
   }
 </style>
+
+<!--
+XS
+S
+M
+L
+XL
+32
+34
+36
+38
+40
+42
+44
+One Size
+-->
