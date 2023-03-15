7cc2c15910d21dc0d1a31c2409fdadc4c46cd6d3
Pontus Norén Stomberg <pontus_noren91@hotmail.com>
2023-03-15 11:25:22 +0100

    updates to journal

--- src/views/JournalView.vue
+++ src/views/JournalView.vue
@@ -10,7 +10,7 @@
 
 <template>
   <div>
-    <div class="flex justify-between items-center">
+    <div class="componentTitle flex justify-between">
       <h1>Journal</h1>
       <router-link :to="`/NewJournalView/`"><button class="button-68 greenW">New Journal</button></router-link>
     </div>
@@ -29,13 +29,13 @@
         <tbody class="divide-y divide-gray-500">
           <tr v-for="journal in journals" :key="journal.id">
             <td v-shortText="{ text: journal.title, chars: 10 }">{{ journal.title }}</td>
-            <td>{{ journal.subtitle.slice(0, 10) }}</td>
-            <td v-shortText="{ text: journal.content, chars: 30 }">{{ journal.content }}</td>
+            <td>{{ journal.subtitle.slice(0, 10) }}...</td>
+            <td>{{ journal.content.slice(0,30) }}...</td>
             <!-- v-html="journal.content" -->
-            <td>{{ journal.tags }}</td>
+            <td>{{ journal.tags.slice(0,20) }}...</td>
             <td><img class="w-24" :src="journal.image" alt="Journal picture" /></td>
             <td>{{ new Date(journal.created_at).toLocaleString('sv-SE', { timeZone: 'GMT' }) }}</td>
-            <button @click="$router.push({ name: 'EditJournal', params: { id: journal.id } })">Edit</button>
+            <td class="flex items-center justify-end gap-10"><button class="cursor-pointer border border-black py-2 px-6 rounded-3xl hover:bg-gray-800 hover:text-white transition-all" @click="$router.push({ name: 'EditJournal', params: { id: journal.id } })">Edit</button></td>
           </tr>
         </tbody>
       </table>
